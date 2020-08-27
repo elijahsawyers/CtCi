54e2f47b680c82064d0a0195b39021cb10964cd8
Elijah Sawyers <elijahsawyers@gmail.com>
2020-08-26 23:46:33 -0500

    More Arrays & Strings

--- Arrays & Strings/RotateMatrix.playground/Contents.swift
+++ Arrays & Strings/RotateMatrix.playground/Contents.swift
@@ -14,10 +14,41 @@ Solutions 2 is presented below.
 import SwiftUI
 
 func rotate<T>(_ matrix: inout [[T]], _ n: Int) {
-    
-    for i in 0..<n {
-        for j in i..<n {
-            // TODO
+    for i in 0..<n/2 {
+        for j in i..<n-1 {
+            let tmp = matrix[i][j]
+
+            for k in 0..<4 {
+                switch k {
+                case 0:
+                    matrix[i][j] = matrix[n - 1 - j][i]
+                case 1:
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                case 2:
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                case 3:
+                    matrix[j][n - 1 - i] = tmp
+                default:
+                    break
+                }
+            }
         }
     }
 }
+
+func main() {
+    var test = [
+        [01, 02, 03, 04],
+        [05, 06, 07, 08],
+        [09, 10, 11, 12],
+        [13, 14, 15, 16]
+    ]
+    
+    rotate(&test, 4)
+    
+    for row in test {
+        print(row)
+    }
+}
+
+main()
